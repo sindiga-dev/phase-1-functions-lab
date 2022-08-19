9d598bb569b9668d1f017a469b011baf46c67794
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-19 11:50:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    let hq=42;
+    return Math.abs(hq-location);
+}
+function distanceFromHqInFeet(location){
+    let hq=42;
+    return Math.abs(hq-location)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination-start)*264;
+}
+function calculatesFarePrice(start, destination){
+    let distance=Math.abs(destination-start)*264;
+    if (distance>2500){
+        return "cannot travel that far";
+    }
+    else if (distance>2000){
+        return 25;
+    }
+    else if(distance>400) {
+        return 2.56;
+    }
+    else{
+        return 0;
+    }
+}
